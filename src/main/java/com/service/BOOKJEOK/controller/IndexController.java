1142fdbf72c85363f9aaa1a9f0bc3bdd576b8dd7
mjh851819 <mjh851819@gmail.com>
2023-09-20 09:17:32 +0900

    db connect

--- src/main/java/com/service/BOOKJEOK/controller/IndexController.java
+++ src/main/java/com/service/BOOKJEOK/controller/IndexController.java
@@ -1,15 +1,10 @@
-package com.service.BJBJ.controller;
+package com.service.BOOKJEOK.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-
-    @Autowired
-    private Environment env;
 
     @GetMapping
     public String index(){
