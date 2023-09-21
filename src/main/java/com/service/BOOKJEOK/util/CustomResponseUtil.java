25dffe999cc7d6b7ace5721654927a19adfb6079
mjh851819 <mjh851819@gmail.com>
2023-09-21 21:03:44 +0900

    [fix] PR과정에서 발생한 오타 수정

--- src/main/java/com/service/BOOKJEOK/util/CustomResponseUtil.java
+++ src/main/java/com/service/BOOKJEOK/util/CustomResponseUtil.java
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 package com.service.BOOKJEOK.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.service.BOOKJEOK.dto.ExceptionResponseDto;
-=======
-package com.mjh8518.bank.util;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mjh8518.bank.dto.ResponseDto;
->>>>>>> 36f437db1594839a77a680395ed75ec9deabc918
+import com.service.BOOKJEOK.dto.ExceptionResponseDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,22 +15,14 @@ public class CustomResponseUtil {
     public static void unAuthentication(HttpServletResponse response, String msg) {
         try {
             ObjectMapper om = new ObjectMapper();
-<<<<<<< HEAD
             ExceptionResponseDto responseDto = new ExceptionResponseDto(-1, msg, null);
-=======
-            ResponseDto responseDto = new ResponseDto<>(-1, msg, null);
->>>>>>> 36f437db1594839a77a680395ed75ec9deabc918
             String responseBody = om.writeValueAsString(responseDto);
             response.setContentType("application/json; charset=utf-8");
             response.setStatus(401);
             //response.getWriter().println("error"); // 모든 요청의 응답에서 "error" 로 일관성 있는 응답을 만들었다. dto로 깔끔하게 만들어보자.
             response.getWriter().println(responseBody);
         } catch (Exception e){
-<<<<<<< HEAD
-            log.error("Server Parsing Error");
-=======
             log.error("서버 파싱 에러");
->>>>>>> 36f437db1594839a77a680395ed75ec9deabc918
         }
 
     }
