edfc665408ae7325825dca6bead681e847f105f0
mjh851819 <mjh851819@gmail.com>
2023-09-24 23:29:46 +0900

    [feat] UserRepository findByEmail 추가

---
+++ src/main/java/com/service/BOOKJEOK/util/authorityUtils.java
@@ -0,0 +1,14 @@
+package com.service.BOOKJEOK.util;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class authorityUtils {
+
+    public List<GrantedAuthority> createAuth(){
+
+    }
+}
