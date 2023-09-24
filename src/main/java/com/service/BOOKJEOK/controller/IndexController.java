4e432fc8f987d0745d7d2927fe5f43dfe90a0f31
mjh851819 <mjh851819@gmail.com>
2023-09-25 01:19:48 +0900

    [feat] test용 controller api 추가 (삭제 예정)

--- src/main/java/com/service/BOOKJEOK/controller/IndexController.java
+++ src/main/java/com/service/BOOKJEOK/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.service.BOOKJEOK.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +13,17 @@ public class IndexController {
     @GetMapping
     public String index(){
         return "index";
+    }
+
+
+    @GetMapping("/tokenTest")
+    public String tokenTest(@RequestParam String access_token, @RequestParam String refresh_token) {
+        return access_token;
+    }
+
+    @GetMapping("/auth")
+    public String auth(){
+        return "Ok";
     }
 
 }
