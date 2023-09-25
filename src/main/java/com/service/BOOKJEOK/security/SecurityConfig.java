dc3feffbb74ba24868a5ca62b868202d7671bfef
mjh851819 <mjh851819@gmail.com>
2023-09-26 01:09:56 +0900

    [feat] JwtError & JwtException 추가 / SecurityConfig 등록

--- src/main/java/com/service/BOOKJEOK/security/SecurityConfig.java
+++ src/main/java/com/service/BOOKJEOK/security/SecurityConfig.java
@@ -1,10 +1,10 @@
-package com.service.BOOKJEOK.config;
+package com.service.BOOKJEOK.security;
 
-import com.service.BOOKJEOK.config.Auth.JwtAuthorizationFilter;
-import com.service.BOOKJEOK.config.oauth.CustomOAuth2UserService;
-import com.service.BOOKJEOK.config.oauth.OAuth2SuccessHandler;
-import com.service.BOOKJEOK.domain.UserEnum;
-import com.service.BOOKJEOK.userService.UserService;
+import com.service.BOOKJEOK.security.filter.JwtAuthorizationFilter;
+import com.service.BOOKJEOK.security.jwt.JwtService;
+import com.service.BOOKJEOK.security.jwt.JwtVO;
+import com.service.BOOKJEOK.security.oauth.OAuth2SuccessHandler;
+import com.service.BOOKJEOK.service.UserService;
 import com.service.BOOKJEOK.util.CustomResponseUtil;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -16,7 +16,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -32,6 +31,7 @@ public class SecurityConfig {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
     private final UserService userService;
+    private final JwtService jwtService;
     private final OAuth2UserService customOAuth2UserService;
 
     @Bean
@@ -47,7 +47,7 @@ public class SecurityConfig {
         public void configure(HttpSecurity builder) throws Exception {
             AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
             //builder.addFilter(new JwtAuthenticationFilter(authenticationManager));
-            builder.addFilter(new JwtAuthorizationFilter(authenticationManager));
+            builder.addFilter(new JwtAuthorizationFilter(authenticationManager, jwtService));
         }
     }
 
@@ -66,13 +66,20 @@ public class SecurityConfig {
          * 기존 SpringBoot에서 Security 관련 예외가 터지면 ExceptionTranslationFilter가 예외를 가로채서 Http나 json에 맞게 예외를 출력해준다.
          * 아래 메소드로 ExceptionTranslationFilter의 제어권을 빼앗아서 일관성있는 예외 출력을 구현한다.
          * */
-        http.exceptionHandling().authenticationEntryPoint((request, response, outhException) -> {
-            CustomResponseUtil.fail(response, "로그인을 진행해 주세요", HttpStatus.UNAUTHORIZED);
+        http.exceptionHandling().authenticationEntryPoint((request, response, oauthException) -> {
+            if(request.getAttribute(JwtVO.EXCEPTION) != null) {
+                CustomResponseUtil.fail(response, request.getAttribute(JwtVO.EXCEPTION).toString(), HttpStatus.UNAUTHORIZED);
+            }
+            else{
+                CustomResponseUtil.fail(response, "로그인이 필요합니다.", HttpStatus.UNAUTHORIZED);
+            }
+
         });
         // 권한 실패에 대한 핸들링
         http.exceptionHandling().accessDeniedHandler(((request, response, accessDeniedException) -> {
-            CustomResponseUtil.fail(response, "권한이 없습니다.", HttpStatus.FORBIDDEN);
+            CustomResponseUtil.fail(response, "접근 권한이 없습니다.", HttpStatus.FORBIDDEN);
         }));
+
 
 
         //JSessionId를 서버에서 관리하지 않겠다.
@@ -91,7 +98,7 @@ public class SecurityConfig {
                 .anyRequest().permitAll();
 
         http.oauth2Login(oauth2 -> oauth2
-                .successHandler(new OAuth2SuccessHandler(userService))
+                .successHandler(new OAuth2SuccessHandler(jwtService))
                 .userInfoEndpoint()
                 .userService(customOAuth2UserService));
 
