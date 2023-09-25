c19d4b20fedf18454fded95939bd05863267cc48
mjh851819 <mjh851819@gmail.com>
2023-09-26 01:13:27 +0900

    [fix] 기능 추가로 인한 OAuth2SuccessHandler 로직 변경

--- src/main/java/com/service/BOOKJEOK/security/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/service/BOOKJEOK/security/oauth/OAuth2SuccessHandler.java
@@ -1,7 +1,9 @@
-package com.service.BOOKJEOK.config.oauth;
+package com.service.BOOKJEOK.security.oauth;
 
 import com.service.BOOKJEOK.domain.User;
-import com.service.BOOKJEOK.userService.UserService;
+import com.service.BOOKJEOK.security.dto.CustomOauth2UserDetails;
+import com.service.BOOKJEOK.security.jwt.JwtService;
+import com.service.BOOKJEOK.security.jwt.JwtVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -14,33 +16,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URI;
-import java.util.Optional;
-
-import static com.service.BOOKJEOK.config.jwt.JwtProcess.delegateAccessToken;
-import static com.service.BOOKJEOK.config.jwt.JwtProcess.delegateRefreshToken;
 
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-    private final UserService userService;
+    private final JwtService jwtService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+        CustomOauth2UserDetails oAuth2User = (CustomOauth2UserDetails) authentication.getPrincipal();
 
         redirect(request, response, oAuth2User);  // Access Token과 Refresh Token을 Frontend에 전달하기 위해 Redirect
     }
 
-    private void redirect(HttpServletRequest request, HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException {
+    private void redirect(HttpServletRequest request, HttpServletResponse response, CustomOauth2UserDetails oAuth2User) throws IOException {
         //log.info("Token 생성 시작");
         String email = oAuth2User.getUser().getEmail();
 
-        String accessToken = delegateAccessToken(email, oAuth2User.getAuthorities());  // Access Token 생성
-        String refreshToken = delegateRefreshToken(email, oAuth2User.getAuthorities());     // Refresh Token 생성
-        Optional<User> target = userService.findByEmail(email);
+        String accessToken = jwtService.createAccessToken(oAuth2User.getUser());  // Access Token 생성
+        String refreshToken = jwtService.createRefreshToken();     // Refresh Token 생성
         //인증 성공 로직이기 때문에 반드시 객체가 존재한다.
-        User user = target.get();
-        String username = user.getName();
-        user.setRefreshToken(refreshToken);
+        User user = jwtService.getUserByEmail(email);
+        jwtService.setRefreshTokenToUser(user, refreshToken);
 
         String uri = createURI(accessToken, refreshToken).toString();   // Access Token과 Refresh Token을 포함한 URL을 생성
         getRedirectStrategy().sendRedirect(request, response, uri);   // sendRedirect() 메서드를 이용해 Frontend 애플리케이션 쪽으로 리다이렉트
@@ -49,8 +45,8 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     // Redirect URI 생성. JWT를 쿼리 파라미터로 담아 전달한다.
     private URI createURI(String accessToken, String refreshToken) {
         MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
-        queryParams.add("access_token", accessToken);
-        queryParams.add("refresh_token", refreshToken);
+        queryParams.add(JwtVO.ACCESS_TOKEN,JwtVO.TOKEN_PREFIX +  accessToken);
+        queryParams.add(JwtVO.REFRESH_TOKEN,JwtVO.TOKEN_PREFIX +  refreshToken);
 
         return UriComponentsBuilder
                 .newInstance()
