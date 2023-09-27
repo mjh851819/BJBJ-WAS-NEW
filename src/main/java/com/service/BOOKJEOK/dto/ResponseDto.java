e8c18bca1cf21c4573bc2728890b4c30a6ab41c1
mjh851819 <mjh851819@gmail.com>
2023-09-27 22:30:47 +0900

    [fix] ExceptionResponseDto -> ResponseDto 이름 변경 / 리팩토링

--- src/main/java/com/service/BOOKJEOK/dto/ResponseDto.java
+++ src/main/java/com/service/BOOKJEOK/dto/ResponseDto.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
-public class ExceptionResponseDto<T> {
+public class ResponseDto<T> {
 
     private final Integer code; // 1 성공, -1 실패
     private final String msg;
