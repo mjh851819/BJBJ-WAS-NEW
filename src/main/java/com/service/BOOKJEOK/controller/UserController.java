2769b1d9efcb0572eb20f77680c488e78d8e4b1b
mjh851819 <mjh851819@gmail.com>
2023-09-27 22:35:50 +0900

    [feat] UserController 기능 추가

---
+++ src/main/java/com/service/BOOKJEOK/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.service.BOOKJEOK.controller;
+
+import com.service.BOOKJEOK.domain.User;
+import com.service.BOOKJEOK.dto.ResponseDto;
+import com.service.BOOKJEOK.dto.user.UserResponseDto;
+import com.service.BOOKJEOK.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.service.BOOKJEOK.dto.user.UserResponseDto.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(final UserService userService) {
+        this.userService = userService;
+    }
+
+    //유저 정보 상세 조회
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> searchUser(
+            @PathVariable final Long userId) {
+        UserSearchResDto userSearchResDto = userService.searchUser(userId);
+
+        return new ResponseEntity<>(new ResponseDto<>(1, "유저 검색 성공", userSearchResDto), HttpStatus.OK);
+    }
+
+}
