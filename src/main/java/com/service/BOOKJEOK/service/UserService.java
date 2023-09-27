46b8b848b07f211ca36e4f99562e8eecdeb70e9a
mjh851819 <mjh851819@gmail.com>
2023-09-27 22:36:27 +0900

    [fix] searchUser 메서드 구조 변경

--- src/main/java/com/service/BOOKJEOK/service/UserService.java
+++ src/main/java/com/service/BOOKJEOK/service/UserService.java
@@ -1,9 +1,15 @@
 package com.service.BOOKJEOK.service;
 
 import com.service.BOOKJEOK.domain.User;
+import com.service.BOOKJEOK.dto.user.UserResponseDto;
+import com.service.BOOKJEOK.handler.ex.CustomApiException;
 import com.service.BOOKJEOK.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static com.service.BOOKJEOK.dto.user.UserResponseDto.*;
 
 @Service
 @Transactional(readOnly = true)
@@ -15,11 +21,17 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User searchUser(Long userId) {
-        return userRepository.findById(userId)
-                .orElse(null);
+    public UserSearchResDto searchUser(Long userId) {
+        Optional<User> userPS = userRepository.findById(userId);
+
+        if(userPS.isEmpty()){
+            throw new CustomApiException("유저를 찾을 수 없습니다.");
+        }
+
+        return new UserSearchResDto(userPS.get());
     }
 
+    //OAuth로 인한 자동 회원가입 / 로그인 이기 때문에 현재는 사용되지 않음
     @Transactional
     public User createUser(User user) {
         return userRepository.save(user);
