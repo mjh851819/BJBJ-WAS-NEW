7512a046a9754b7f44c3754fdfd6aa020d2c629d
mjh851819 <mjh851819@gmail.com>
2023-10-02 22:02:32 +0900

    [refactor] Controller 불필요한 코드 수정

--- src/main/java/com/service/BOOKJEOK/controller/IndexController.java
+++ src/main/java/com/service/BOOKJEOK/controller/IndexController.java
@@ -4,14 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.service.BOOKJEOK.dto.club.ClubRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
