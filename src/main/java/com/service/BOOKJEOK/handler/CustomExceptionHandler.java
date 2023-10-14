5ab05ff4aa7d0f5db3dbf0eef06b4678a162b165
mjh851819 <mjh851819@gmail.com>
2023-10-14 10:28:35 +0900

    [fix] 400 오류 api 응답 설정

--- src/main/java/com/service/BOOKJEOK/handler/CustomExceptionHandler.java
+++ src/main/java/com/service/BOOKJEOK/handler/CustomExceptionHandler.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,4 +20,11 @@ public class CustomExceptionHandler {
         log.error(e.getMessage());
         return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<?> MissingServletRequestParameterException(MissingServletRequestParameterException e){
+        log.error(e.getMessage());
+        return new ResponseEntity<>(new ResponseDto<>(-1, "잘못된 파라미터 입력입니다.", null), HttpStatus.BAD_REQUEST);
+    }
+
 }
